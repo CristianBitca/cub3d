f2d0d32305208a75125ff1b226acc1ae336d5257
tepyam <teppei.yamano@gmail.com>
2026-02-21 15:31:07 +0000

    updated raycasting_ty.c

--- src/render/raycasting_ty.c
+++ src/render/raycasting_ty.c
@@ -14,6 +14,37 @@
 #include "render.h"
 #include "parsing.h"
 
+void	draw_wall_stripe(t_game, *game, t_ray *ray, int *draw_start, int *draw_end)
+{
+	int	y;
+
+	y = *draw_start;
+	while (y < *draw_end)
+	{
+		if (ray->side == 0)
+			put_pixel(game->img, x, y, RED);
+		else
+			put_pixel(game->img, x, y, GREEN);
+		y++;
+	}
+}
+
+void	get_dist_and_height(t_game *game, t_ray *ray, int *draw_start, int *draw_end)
+{
+	int	line_height;
+
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->map_x - game->player->x + (1 - ray->step_x) / 2) / ray->dir_x;
+	else
+		ray->perp_wall_dist = (ray->map_y - game->player->y + (1 - ray->step_y) / 2) / ray->dir_y;
+	line_height = (int)(game->screen_height / ray.perp_wall_dist);
+	*draw_start = -line_height / 2 + game->screen_height / 2;
+	if (*draw_start < 0)
+		*draw_start = 0;
+	*draw_end = line_height / 2 + game->screen_height / 2;
+	if (*draw_end >= game->screen_height)
+		*draw_end = game->screen_height - 1;
+}
 
 void	perform_dda(t_game *game, t_ray *ray)
 {
@@ -86,17 +117,16 @@ void	raycast(t_game *game)
 {
 	int 	x;
 	t_ray	ray;
+	int		draw_start;
+	int		draw_end;
 	
 	x = -1;
 	while (++x < game->screen_width)
 	{
-		// 1. Calculate ray direction
-		// 2. Initialise ray position
 		init_ray(game, x, &ray);
-		// 3. perform DDA
 		perform_dda(game, &ray);
-		// 4. Calculate perpendicular distance
-		// 5. Calculate line height
-		// 6. Draw vertical stripe
+		get_dist_and_height(game, &ray, &draw_start, &draw_end);
+		draw_wall_stripe(game, &ray, &draw_start, &draw_end);
+		x++;
 	}
 }
